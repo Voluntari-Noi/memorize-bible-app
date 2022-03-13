8d1f47aaaf08e1a10d86dcb621109ffad50f549f
GhitaB <ghitabizau27@gmail.com>
2022-03-13 10:02:27 +0200

    Copy structure from questions app.

---
+++ script.js
@@ -0,0 +1,24 @@
+$('document').ready(function() {
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  $("button.btn-more").on('click', function() {
+    $("div.row-footer").toggle();
+  });
+
+  function start_game() {
+    alert("Yey");
+  }
+
+  start_game();
+});
